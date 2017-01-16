4d0995814712eaf3968f51d84bf69f9aac3eae8a
Curt White <curt3n5@gmail.com>
2017-01-16 18:59:18 -0500

    Nordic nRF51822 Pin Testing Utility for mbed
    Nordic nRF51822 Bluetooth BLE / ARM M0 SoC
    Blinks selects pins for multimeter pickup

---
+++ main.cpp
@@ -0,0 +1,140 @@
+#include "mbed.h"
+
+#define PIN_00  p0
+#define PIN_01  p1
+#define PIN_02  p2
+#define PIN_03  p3
+#define PIN_04  p4
+#define PIN_05  p5
+#define PIN_06  p6
+#define PIN_07  p7
+#define PIN_08  p8
+#define PIN_09  p9  
+#define PIN_10  p10  
+#define PIN_11  p11
+#define PIN_12  p12
+#define PIN_13  p13
+#define PIN_14  p14
+#define PIN_15  p15
+#define PIN_16  p16
+#define PIN_17  p17  
+#define PIN_18  p18
+#define PIN_19  p19  
+#define PIN_20  p20
+#define PIN_21  p21  
+#define PIN_22  p22  
+#define PIN_23  p23  
+#define PIN_24  p24 
+#define PIN_25  p25
+#define PIN_26  p26
+#define PIN_27  p27
+#define PIN_28  p28
+#define PIN_29  p29
+#define PIN_30  p30
+
+DigitalOut pin_00(PIN_00);
+DigitalOut pin_01(PIN_01);
+DigitalOut pin_02(PIN_02);
+DigitalOut pin_03(PIN_03);
+DigitalOut pin_04(PIN_04);
+DigitalOut pin_05(PIN_05);
+DigitalOut pin_06(PIN_06);
+DigitalOut pin_07(PIN_07);
+DigitalOut pin_08(PIN_08);
+DigitalOut pin_09(PIN_09);
+DigitalOut pin_10(PIN_10);
+DigitalOut pin_11(PIN_11);
+DigitalOut pin_12(PIN_12);
+DigitalOut pin_13(PIN_13);
+DigitalOut pin_14(PIN_14);
+DigitalOut pin_15(PIN_15);
+DigitalOut pin_16(PIN_16);
+DigitalOut pin_17(PIN_17);
+DigitalOut pin_18(PIN_18);
+DigitalOut pin_19(PIN_19);
+DigitalOut pin_20(PIN_20);
+DigitalOut pin_21(PIN_21);
+DigitalOut pin_22(PIN_22);
+DigitalOut pin_23(PIN_23);
+DigitalOut pin_24(PIN_24);
+DigitalOut pin_25(PIN_25);
+DigitalOut pin_26(PIN_26);
+DigitalOut pin_27(PIN_27);
+DigitalOut pin_28(PIN_28);
+DigitalOut pin_29(PIN_29);
+DigitalOut pin_30(PIN_30);
+
+
+int main() {
+    while(1) {       
+   //     pin_00 = 1;
+   //     pin_01 = 1;
+   //     pin_02 = 1;
+   //     pin_03 = 1;
+   //     pin_04 = 1; 
+        pin_05 = 1; 
+   //     pin_06 = 1; 
+   //     pin_07 = 1; 
+   //     pin_08 = 1;   
+   //     pin_09 = 1;
+   //     pin_10 = 1;
+   //     pin_11 = 1;
+   //     pin_12 = 1;
+   //     pin_13 = 1;
+   //     pin_14 = 1;
+   //     pin_15 = 1; 
+   //     pin_16 = 1;   
+   //     pin_17 = 1;
+   //     pin_18 = 1;   
+   //     pin_19 = 1;
+   //     pin_20 = 1;   
+   //     pin_21 = 1;
+   //     pin_22 = 1;
+   //     pin_23 = 1;
+   //     pin_24 = 1;
+   //     pin_25 = 1;
+   //     pin_26 = 1;
+   //     pin_27 = 1;
+   //     pin_28 = 1;
+   //     pin_29 = 1;
+   //     pin_30 = 1;   
+           
+        
+        wait(1);
+        
+ 
+        pin_00 = 0;
+        pin_01 = 0;
+        pin_02 = 0;
+        pin_03 = 0;
+        pin_04 = 0;
+        pin_05 = 0;
+        pin_06 = 0;
+        pin_07 = 0;
+        pin_08 = 0;
+        pin_09 = 0;
+        pin_10 = 0;
+        pin_11 = 0;
+        pin_12 = 0;
+        pin_13 = 0;
+        pin_14 = 0;
+        pin_15 = 0;
+        pin_16 = 0;
+        pin_17 = 0;
+        pin_18 = 0;
+        pin_19 = 0;
+        pin_20 = 0;
+        pin_21 = 0;
+        pin_22 = 0;
+        pin_23 = 0;
+        pin_24 = 0;
+        pin_25 = 0;
+        pin_26 = 0;
+        pin_27 = 0;
+        pin_28 = 0;
+        pin_29 = 0;
+        pin_30 = 0;        
+        
+        wait(1);
+    }
+}
